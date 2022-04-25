8fcf013edd61f3fbc0350cea99a0f565f08b2e08
Clmtg <garciaclement.c@icloud.com>
2022-04-25 16:10:44 +0200

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Clément Garcia on 25/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
